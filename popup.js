ec8dafcec860328b7b9ab4d38d5758990cbf91b1
Matias Agelvis <Magelvisdoz@gmail.com>
2021-09-25 20:37:38 -0400

    ✨ Added scoring on search pages

--- popup.js
+++ popup.js
@@ -22,6 +22,17 @@ async function setPageBackgroundColor() {
     // let url = window.location.toString()
     console.log('\n---------\n');
 
+    // add google material icons
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+    document.head.appendChild(link);
+
+
+    // START entry point to the algorithm
+    AntonEgo(...prepPathname(window.location.pathname));
+
+
     // console.log(url);
     function last(array) {
         // returns the last element of the given array
@@ -30,7 +41,7 @@ async function setPageBackgroundColor() {
 
     function count(str, pattern) {
         // returns the number of marches of a pattern in a string
-        const re = RegExp(pattern, 'g')
+        let re = RegExp(pattern, 'g')
         return ((str || '').match(re) || []).length
     }
 
@@ -46,7 +57,7 @@ async function setPageBackgroundColor() {
         // returns (currently) the age of the review in days since the Unix Epoch
         let daysFactor = 1000 * 3600 * 24;
 
-        var date = Date.parse(review.getElementsByClassName('dateOfReview')[0].innerText);
+        let date = Date.parse(review.getElementsByClassName('dateOfReview')[0].innerText);
 
         return date / daysFactor;
     }
@@ -54,21 +65,21 @@ async function setPageBackgroundColor() {
     function countLikes(review) {
         // returns the number of thumbs up (this was helpful) of a given review
         let baseReviewValue = 0;
-        var likes = review.innerHTML.match(/<span>This is helpful \((.*)\)<\/span>/);
+        let likes = review.innerHTML.match(/<span>This is helpful \((.*)\)<\/span>/);
         return baseReviewValue + (likes ? parseInt(likes[1]) : 0);
     }
 
     function peerScore(reviews) {
         // returns the weighted average of the peer review valuation model
-        var score = 0;
-        var totalWeight = 0;
+        let score = 0;
+        let totalWeight = 0;
         let peerPower = 2;
         let timedecay = 0.3;
 
         reviews.forEach(review => {
             // console.log(countStars(review))
             // console.log(countLikes(review))
-            // calculate the weighted average
+            // calculate the score
 
             let peers = countLikes(review) ** peerPower
             let time = getAgeOfReview(review) ** timedecay;
@@ -83,21 +94,9 @@ async function setPageBackgroundColor() {
         return score;
     }
 
-    function getReviews(html) {
-        // returns the top 25 reviews in an array
-        // Convert the HTML string into a document object
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, 'text/html');
-
-        var reviews = doc.getElementsByClassName('review');
-
-        // returns an array so that is easier to iterate over later on
-        return Array.from(reviews);
-    }
-
     function getCourseraScore() {
         // returns the official coursera valuation
-        var score = document.getElementsByClassName('rating-text')[0].innerText;
+        let score = document.getElementsByClassName('rating-text')[0].innerText;
         // return parseFloat(score);
         // for security we match the number in regex before parsing it
         return parseFloat(score.match(/[\d\.]+/));
@@ -105,7 +104,7 @@ async function setPageBackgroundColor() {
 
     function getNumberRatings() {
         // returns the total number of reviews of the course in coursera
-        var count = document.querySelector('[data-test="ratings-count-without-asterisks"]').innerText;
+        let count = document.querySelector('[data-test="ratings-count-without-asterisks"]').innerText;
         // we must remove the thousend comma separator
         return parseInt(count.replace(/,/g, ''));
     }
@@ -118,67 +117,88 @@ async function setPageBackgroundColor() {
 
 
     async function fetchReviewPage(course) {
-        response = await fetch('https://www.coursera.org/learn/' + course + '/reviews');
-        
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(await response.text(), 'text/html');
+        // given a course name will return 
+        // the parsed doc of the review page for the course
+
+        let response;
+
+        try {
+            response = await fetch('https://www.coursera.org/learn/' + course + '/reviews');
+        } catch (error) {
+            console.error(error);
+        }
+
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(await response.text(), 'text/html');
 
         return doc;
     }
 
 
-    function getCourseReviews(course) {
+    async function getCourseReviews(course) {
         // we are strict with the url to simplify cases where the request
         // is made from the very reviews page of the course
-        fetch('https://www.coursera.org/learn/' + course + '/reviews').then(function(response) {
-            // The API call was successful!
-            return response.text();
-        }).then(function(html) {
 
-            // the peer reviewed reviews get to decide 4 stars
-            let peerStars = 4 / 5;
-            // the rest depends on the agregate from coursera
-            let voxPopuliStars = 1 - peerStars;
+        let doc = await fetchReviewPage(course);
 
+        // an array so that is easier to iterate over later on
+        let reviews = Array.from(doc.getElementsByClassName('review'));
 
-            var reviews = getReviews(html);
-            var score = peerStars * peerScore(reviews) + voxPopuliStars * getCourseraScore();
+        // the peer reviewed reviews get to decide 4 stars
+        let peerStars = 4 / 5;
+        // the rest depends on the agregate from coursera
+        let voxPopuliStars = 1 - peerStars;
 
-            // round to one decimal place
-            score = score.toFixed(1);
+        let score = peerStars * peerScore(reviews) + voxPopuliStars * getCourseraScore();
 
+        // round to one decimal place
+        score = score.toFixed(1);
 
-            console.log('Real adjusted score of the course:', score);
-
-            // here will go the style modification
-            // we will make to the course valuation
-
-
-        }).catch(function(err) {
-            // There was an error
-            console.warn('Something went wrong.', err);
-        });
-
+        return score;
     }
 
-
-    let url = window.location.pathname.split('/')
-
-    // check if we are in a search or in the page of a course
-    switch (url[1]) {
-        case 'search':
-            console.log('this is a search page, implementation comming soon');
-            break;
-        case 'learn':
-            console.log(url[2]);
-            let a = await fetchReviewPage(url[2]);
-            console.log((await fetchReviewPage(url[2])).getElementsByClassName('review'));
-            console.log(a.getElementsByClassName('review'));
-            getCourseReviews(url[2]);
-            break;
-        default:
-            console.log('Deafult case');
+    function prepPathname(pathname){
+        return [pathname.split('/')[1], pathname.split('/')[2]]
     }
 
+    async function AntonEgo(type, name) {
+
+        // check if we are in a search or in the page of a course
+        switch (type) {
+            case 'search':
+                // SEARCH PAGE, must call this function for each element
+                // No visual modifications should be made at this level
+
+                console.log('this is a search page, implementation comming soon');
+                // if we are in a search page we then document has all the data we need
+                let results = Array.from(document.getElementsByClassName('result-title-link'));
+                
+                // turn array of a tags to their pathnames
+                let pathnames = results.map(x => x.pathname);
+                // prepare path names
+                pathnames = pathnames.map(prepPathname);
+
+                let scores = pathnames.map(x => AntonEgo(...x))
+                console.log(scores)
+
+                break;
+            case 'learn':
+                // COURSE, base case
+                console.log(name);
+                let score = await getCourseReviews(name);
+                console.log('Adjusted score of the course:', score);
+                break;
+
+            case 'professional-certificates':
+            case 'specializations':
+                console.log('this is a specialization or professional-certificate page, implementation comming soon');
+                // professional certificates consist of several courses
+                // must score them all and average
+                break;
+
+            default:
+                console.log('Deafult case');
+        }
+    }
 
 }
