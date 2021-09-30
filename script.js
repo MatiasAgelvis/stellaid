1ae6b504dee913c1801c83a38c83e7e251c48164
Matias Agelvis <Magelvisdoz@gmail.com>
2021-09-30 09:08:24 -0400

    🎨 Changed structure from namespaces to classes
    Added verification for  courses without reviews
    
    Added method to display the score of courses

--- script.js
+++ script.js
@@ -1,183 +1,239 @@
-var reviewer = {
-    // console.log(url);
-    last: function(array) {
+// the hackiest solution I could find
+{
+    var icon = '<i style=\'font-size: inherit; line-height: unset; vertical-align: bottom;\' class="material-icons">verified</i>'
+    // var icon = '<i class="bi bi-patch-check"></i>'
+
+    function zip(arrays) {
+        return arrays[0].map(function(_, i) {
+            return arrays.map(function(array) { return array[i] })
+        });
+    }
+
+    function last(array) {
         // returns the last element of the given array
         return array[array.length - 1];
-    },
+    }
 
-    count: function(str, pattern) {
+
+    function regexCount(str, pattern) {
         // returns the number of marches of a pattern in a string
         let re = RegExp(pattern, 'g')
         return ((str || '').match(re) || []).length
-    },
+    }
 
-    countStars: function(review) {
-        // returns the number of stars given by the reviewer
-        let fullStars = reviewer.count(review.innerHTML, '<title .*?>Filled Star</title>');
-        let halfStars = reviewer.count(review.innerHTML, '<title .*?>Half Star</title>');
-
-        return fullStars + (halfStars / 2);
-    },
-
-    getAgeOfReview: function(review) {
-        // returns (currently) the age of the review in days since the Unix Epoch
-        let daysFactor = 1000 * 3600 * 24;
-
-        let date = Date.parse(review.getElementsByClassName('dateOfReview')[0].innerText);
-
-        return date / daysFactor;
-    },
-
-    countLikes: function(review) {
-        // returns the number of thumbs up (this was helpful) of a given review
-        let baseReviewValue = 0;
-        let likes = review.innerHTML.match(/<span>This is helpful \((.*)\)<\/span>/);
-        return baseReviewValue + (likes ? parseInt(likes[1]) : 0);
-    },
-
-    peerScore: function(doc) {
-        // returns the weighted average of the peer review valuation model
-
-        // an array so that is easier to iterate over later on
-        let reviews = Array.from(doc.getElementsByClassName('review'));
-
-        let score = 0;
-        let totalWeight = 0;
-        let peerPower = 2;
-        let timedecay = 0.3;
-
-        reviews.forEach(review => {
-            // console.log(countStars(review))
-            // console.log(countLikes(review))
-            // calculate the score
-
-            let peers = reviewer.countLikes(review) ** peerPower
-            let time = reviewer.getAgeOfReview(review) ** timedecay;
-
-            score += reviewer.countStars(review) * peers * time;
-            totalWeight += peers * time;
-        });
-
-        // calculate the definitive score of the course
-        score = score / totalWeight;
-
-        return score;
-    },
+    function prepPathname(pathname) { return [pathname.split('/')[1], pathname.split('/')[2]] }
 
 
-    getCourseraScore: function(doc) {
-        // returns the official coursera valuation
-        let score = doc.getElementsByClassName('rating-text')[0].innerText;
-        // return parseFloat(score);
-        // for security we match the number in regex before parsing it
-        return parseFloat(score.match(/[\d\.]+/));
-    },
+    class Course {
 
-    getNumberRatings: function(doc) {
-        // returns the total number of reviews of the course in coursera
-        let count = doc.querySelector('[data-test="ratings-count-without-asterisks"]').innerText;
-        // we must remove the thousend comma separator
-        return parseInt(count.replace(/,/g, ''));
-    },
-
-    courseraScore: function(doc) {
-        // debug func
-        console.log(getCourseraScore(doc));
-        console.log(getNumberRatings(doc));
-    },
-
-
-    fetchReviewPage: async function(course) {
-        // given a course name will return 
-        // the parsed doc of the review page for the course
-
-        let response;
-
-        try {
-            response = await fetch('https://www.coursera.org/learn/' + course + '/reviews');
-        } catch (error) {
-            console.error(error);
+        constructor(name) {
+            this.name = name
+            this.score = null
+            this.hasReviews = true
         }
 
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(await response.text(), 'text/html');
+        async getScore() {
+            this.score = this.getCourseScore(this.name);
+            return this.score;
+        }
 
-        return doc;
-    },
+        countStars(review) {
+            // returns the number of stars given by the reviewer
+            let fullStars = regexCount(review.innerHTML, '<title .*?>Filled Star</title>');
+            let halfStars = regexCount(review.innerHTML, '<title .*?>Half Star</title>');
 
+            return fullStars + (halfStars / 2);
+        }
 
-    getCourseReviews: async function(course) {
-        // we are strict with the url to simplify cases where the request
-        // is made from the very reviews page of the course
+        getAgeOfReview(review) {
+            // returns (currently) the age of the review in days since the Unix Epoch
+            let daysFactor = 1000 * 3600 * 24;
 
-        let doc = await reviewer.fetchReviewPage(course);
+            let date = Date.parse(review.getElementsByClassName('dateOfReview')[0].innerText);
 
-        // the peer reviewed reviews get to decide 4 stars
-        let peerStars = 4 / 5;
-        // the rest depends on the agregate from coursera
-        let voxPopuliStars = 1 - peerStars;
+            return date / daysFactor;
+        }
 
-        let score = peerStars * reviewer.peerScore(doc) + voxPopuliStars * reviewer.getCourseraScore(doc);
-
-        // round to one decimal place
-        score = score.toFixed(1);
-
-        return score;
-    },
-
-    prepPathname: function(pathname) {
-        return [pathname.split('/')[1], pathname.split('/')[2]]
-    },
-}
+        countLikes(review) {
+            // returns the number of thumbs up (this was helpful) of a given review
+            let baseReviewValue = 0;
+            let likes = review.innerHTML.match(/<span>This is helpful \((.*)\)<\/span>/);
+            return baseReviewValue + (likes ? parseInt(likes[1]) : 0);
+        }
 
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-async function setPageBackgroundColor() {
-    // let url = window.location.toString()
-    console.log('\n---------\n');
+        peerScore(doc) {
+            // returns the weighted average of the peer review valuation model
 
-    // add google material icons
-    let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-    document.head.appendChild(link);
+            // an array so that is easier to iterate over later on
+            let reviews = Array.from(doc.getElementsByClassName('review'));
+
+            let score = 0;
+            let totalWeight = 0;
+            let peerPower = 2;
+            let timedecay = 0.3;
+
+            reviews.forEach(review => {
+                // console.log(countStars(review))
+                // console.log(countLikes(review))
+                // calculate the score
+
+                let peers = this.countLikes(review) ** peerPower
+                let time = this.getAgeOfReview(review) ** timedecay;
+
+                score += this.countStars(review) * peers * time;
+                totalWeight += peers * time;
+            });
+
+            // calculate the definitive score of the course
+            score = score / totalWeight;
+
+            return score;
+        }
+
+        getCourseraScore(doc) {
+            // returns the official coursera valuation
+            let score = doc.getElementsByClassName('rating-text')[0].innerText;
+            // return parseFloat(score);
+            // for security we match the number in regex before parsing it
+            return parseFloat(score.match(/[\d\.]+/));
+        }
+
+        getNumberRatings(doc) {
+            // returns the total number of reviews of the course in coursera
+            let total = doc.querySelector('[data-test="ratings-count-without-asterisks"]').innerText;
+            // we must remove the thousend comma separator
+            return parseInt(total.replace(/,/g, ''));
+        }
+
+        async fetchReviewPage(course) {
+            // given a course name will return 
+            // the parsed doc of the review page for the course
+            let response;
+            try {
+                response = await fetch('https://www.coursera.org/learn/' + course + '/reviews');
+            } catch (error) {
+                console.error(error);
+                return null;
+            }
+
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(await response.text(), 'text/html');
+
+            return doc;
+        }
+
+        checkIfReviewed(doc) {
+            this.hasReviews = !(doc.getElementsByClassName('review').length == 0 ||
+                doc.getElementsByClassName('dateOfReview').length == 0 ||
+                doc.getElementsByClassName('rating-text').length == 0)
+
+            return this.hasReviews
+        }
+
+        async getCourseScore(course) {
+            let doc = await this.fetchReviewPage(course);
+
+            if (!this.checkIfReviewed(doc)) { return -1 }
+
+            // the peer reviewed reviews get to decide 4 stars
+            let peerStars = 4 / 5;
+            // the rest depends on the agregate from coursera
+            let voxPopuliStars = 1 - peerStars;
+
+            let score = peerStars * this.peerScore(doc) + voxPopuliStars * this.getCourseraScore(doc);
+
+            // round to one decimal place
+            score = score.toFixed(1);
+
+            return score;
+        }
+
+        async prettylog() {
+            this.score.then(x => console.log(this.name, x))
+        }
+
+        displayResult() {
+            this.score.then(x => {
+        if (x >= 0) { document.getElementsByClassName('rating-text')[0].innerHTML += ` (${x}${icon})` }
+            })
+        }
+    }
+
+    class Search {
+
+        constructor(doc) {
+            this.names = this.getSearchPageCourses(doc);
+            this.courses = []
+        }
+
+        async getScores() {
+            this.courses = this.names.map(x => new Course(x))
+            // await Promise.all(inputArray.map(async (i) => someAsyncFunction(i)));
+            this.courses.forEach(x => x.getScore())
+        }
+
+        getSearchPageCourses(doc) {
+            let results = Array.from(doc.getElementsByClassName('result-title-link'));
+
+            // turn array of a tags to their pathnames
+            let pathnames = results.map(x => x.pathname);
+            // console.log(pathnames)
+
+            // prepare path names
+            return pathnames.map(x => prepPathname(x)[1]);
+        }
+
+        prettylog() {
+            Promise.all(this.courses.map(x => x.score)).then((scores) => {
+
+                for (let i = 0; i < this.names.length; i++) {
+                    console.log(this.names[i], scores[i])
+                }
+            })
+        }
+
+        displayResults() {
+            Promise.all(this.courses.map(x => x.score)).then((scores) => {
+
+                let cards = document.getElementsByClassName('ratings-text');
+                // let containers = document.getElementsByClassName('rc-Ratings')
+                let containers = document.getElementsByClassName('ratings-icon')
+
+                for (let i = 0; i < this.names.length; i++) {
+                    if (scores[i] >= 0) {
+                    cards[i].innerHTML += ` (${scores[i]}${icon})`;
+                    containers[i].style.width = 'unset';
+                    }
+                }
+            })
+        }
+    }
 
 
-    // START entry point to the algorithm
-    AntonEgo(...reviewer.prepPathname(window.location.pathname));
+    function discriminator(type, name) {
 
-    async function AntonEgo(type, name) {
+        console.log('entry type: ', type);
+        console.log('entry name: ', name);
 
+        let X;
         // check if we are in a search or in the page of a course
         switch (type) {
             case 'search':
-                // SEARCH PAGE, must call this function for each element
-                // No visual modifications should be made at this level
-
-                console.log('this is a search page');
+            case 'courses':
+                // console.log('search', search_func)
                 // if we are in a search page we then document has all the data we need
-                let results = Array.from(document.getElementsByClassName('result-title-link'));
-
-                // turn array of a tags to their pathnames
-                let pathnames = results.map(x => x.pathname);
-                // prepare path names
-                pathnames = pathnames.map(reviewer.prepPathname);
-
-                let scores = await Promise.all(pathnames.map(async (x) => AntonEgo(...x)));
-                console.log(scores)
-
-                for (let i = 0; i < scores.length; i++) {
-                    console.log(pathnames[i][1], scores[i])
-                }
-
+                X = new Search(document);
+                X.getScores();
+                X.prettylog();
+                X.displayResults();
                 break;
             case 'learn':
                 // COURSE, base case
-                console.log(name);
-                let score = await reviewer.getCourseReviews(name);
-                console.log('Adjusted score of the course:', score);
-                return await score;
+                X = new Course(name)
+                X.getScore();
+                X.prettylog();
+                X.displayResult();
                 break;
 
             case 'professional-certificates':
@@ -191,6 +247,26 @@ async function setPageBackgroundColor() {
                 console.log('Deafult case');
         }
     }
-}
 
-setPageBackgroundColor();
+
+    // The body of this function will be execuetd as a content script inside the
+    // current page
+    async function main() {
+        // let url = window.location.toString()
+        console.log('\n---------\n');
+
+        // add google material icons
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+        // link.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+        document.head.appendChild(link);
+
+
+        // START entry point to the algorithm
+        discriminator(...prepPathname(window.location.pathname));
+    }
+
+    main();
+
+}
