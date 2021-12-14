739a8aa72501025aac5a3fa539329d766d69162f
Matias Agelvis <magelvisdoz@gmail.com>
2021-12-14 16:25:12 -0400

    downgrade to manifest v2

--- src/background.js
+++ src/background.js
@@ -1,12 +1,6 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    chrome.action.setBadgeText({text: msg.text, tabId: sender.tab.id});
-    chrome.action.setBadgeBackgroundColor({ color: '#4688F1' });
-    sendResponse(msg.text);
-});
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['script.js']
+browser.browserAction.onClicked.addListener(function (tab) {
+    // for the current tab, inject the "inject.js" file & execute it
+    browser.tabs.executeScript(tab.id, {
+        file: 'script.js'
     });
 });
