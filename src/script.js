f3c4d036b0954be9cc66578e3b940ce1caaeab3f
Matias Agelvis <magelvisdoz@gmail.com>
2021-12-14 20:06:54 -0400

    fix manifest.json version and host_permissions

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 // Wraps the whole script in a closure in case the extension is called more than once
 (function() {
     function extensionBadge(msg) {
-        chrome.runtime.sendMessage({ text: msg }, function(response) {
-            // console.log("Badge Text: ", response)
-        })
+        // browser.browserAction.setBadgeText({text: (msg)})
     }
 
     extensionBadge('Wait')
